6ad3e0361d4ac8da0b1cc22910e7437de0abddb4
bdematt <brandon.dematteis@gmail.com>
2025-10-13 22:06:24 +0200

    Shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <emscripten/html5.h>
+#include <webgpu/webgpu_cpp.h>
 
-void main_loop() {
+// Emscripten exposed function, called during window resize
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    void handleResize() {
+        std::cout << "Resizing..." << std::endl;
+    }
 }
 
 int main() {
