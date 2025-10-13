8e22563c05d5272a54b3309dde22cb5a0c22d6f0
bdematt <brandon.dematteis@gmail.com>
2025-10-13 22:47:14 +0200

    Finished Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,162 @@
 #include <iostream>
+#include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
 #include <webgpu/webgpu_cpp.h>
+#include <dawn/webgpu_cpp_print.h>
 
-// Emscripten exposed function, called during window resize
-extern "C" {
-    EMSCRIPTEN_KEEPALIVE
-    void handleResize() {
-        std::cout << "Resizing..." << std::endl;
+
+// Canvas & Window
+uint32_t width = 512;
+uint32_t height = 512;
+
+// WGPU
+wgpu::Instance instance;
+wgpu::Adapter adapter;
+wgpu::Device device;
+wgpu::Surface surface;
+wgpu::TextureFormat format;
+wgpu::RenderPipeline pipeline;
+const char shaderCode[] = R"(
+    @vertex fn vertexMain(@builtin(vertex_index) i : u32) ->
+      @builtin(position) vec4f {
+        const pos = array(vec2f(0, 1), vec2f(-1, -1), vec2f(1, -1));
+        return vec4f(pos[i], 0, 1);
     }
+    @fragment fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(1, 0, 0, 1);
+    }
+)";
+
+void CreateRenderPipeline() {
+  wgpu::ShaderSourceWGSL wgsl{{.code = shaderCode}};
+
+  wgpu::ShaderModuleDescriptor shaderModuleDescriptor{.nextInChain = &wgsl};
+  wgpu::ShaderModule shaderModule =
+      device.CreateShaderModule(&shaderModuleDescriptor);
+
+  wgpu::ColorTargetState colorTargetState{.format = format};
+
+  wgpu::FragmentState fragmentState{
+      .module = shaderModule, .targetCount = 1, .targets = &colorTargetState};
+
+  wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule},
+                                            .fragment = &fragmentState};
+  pipeline = device.CreateRenderPipeline(&descriptor);
+}
+
+
+void setCanvasDimensions() {
+    int iWidth, iHeight;
+    emscripten_get_canvas_element_size("#canvas", &iWidth, &iHeight);
+    width = static_cast<uint32_t>(iWidth);
+    height = static_cast<uint32_t>(iHeight);
+}
+
+void ConfigureSurface() {
+    wgpu::SurfaceCapabilities capabilities;
+    surface.GetCapabilities(adapter, &capabilities);
+    format = capabilities.formats[0];
+
+    wgpu::SurfaceConfiguration config {
+        .device = device,
+        .format = format,
+        .width = width,
+        .height = height,
+        .presentMode = wgpu::PresentMode::Fifo
+    };
+    surface.Configure(&config);
+}
+
+void InitGraphics() {
+    ConfigureSurface();
+    CreateRenderPipeline();
+}
+
+void Render() {
+  wgpu::SurfaceTexture surfaceTexture;
+  surface.GetCurrentTexture(&surfaceTexture);
+
+  wgpu::RenderPassColorAttachment attachment{
+      .view = surfaceTexture.texture.CreateView(),
+      .loadOp = wgpu::LoadOp::Clear,
+      .storeOp = wgpu::StoreOp::Store};
+
+  wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1,
+                                        .colorAttachments = &attachment};
+
+  wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+  wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
+  pass.SetPipeline(pipeline);
+  pass.Draw(3);
+  pass.End();
+  wgpu::CommandBuffer commands = encoder.Finish();
+  device.GetQueue().Submit(1, &commands);
+}
+
+void Init() {
+    // Canvas Dimensions
+    setCanvasDimensions();
+
+    // Create Instance
+    static const auto kTimedWaitAny = wgpu::InstanceFeatureName::TimedWaitAny;
+    wgpu::InstanceDescriptor instanceDesc {
+        .requiredFeatureCount = 1,
+        .requiredFeatures = &kTimedWaitAny
+    };
+    instance = wgpu::CreateInstance(&instanceDesc);
+
+    // Request Adapter
+    wgpu::Future f1 = instance.RequestAdapter(
+        nullptr,
+        wgpu::CallbackMode::WaitAnyOnly,
+        [](wgpu::RequestAdapterStatus status, wgpu::Adapter a, wgpu::StringView message) {
+            if (status != wgpu::RequestAdapterStatus::Success) {
+                std::cout << "RequestAdapter: " << message << "\n";
+                exit(0);
+            }
+            adapter = std::move(a);
+        }
+    );
+    instance.WaitAny(f1, UINT64_MAX);
+
+    // Request Device
+    wgpu::DeviceDescriptor desc{};
+    desc.SetUncapturedErrorCallback(
+        [](const wgpu::Device&, wgpu::ErrorType errorType, wgpu::StringView message) {
+            std::cout << "Error: " << errorType << " - message: " << message << "\n";
+        }
+    );
+    wgpu::Future f2 = adapter.RequestDevice(
+        &desc,
+        wgpu::CallbackMode::WaitAnyOnly,
+        [](wgpu::RequestDeviceStatus status, wgpu::Device d, wgpu::StringView message) {
+            if (status != wgpu::RequestDeviceStatus::Success) {
+                std::cout << "RequestDevice: " << message << "\n";
+                exit(0);
+            }
+            device = std::move(d);
+        }
+    );
+    instance.WaitAny(f2, UINT64_MAX);
+
+    // Create Surface from Canvas
+    wgpu::EmscriptenSurfaceSourceCanvasHTMLSelector canvasDesc{};
+    canvasDesc.selector = "#canvas";
+
+    wgpu::SurfaceDescriptor surfaceDesc{};
+    surfaceDesc.nextInChain = &canvasDesc;
+    surface = instance.CreateSurface(&surfaceDesc);
+
+    InitGraphics();
+}
+
+void MainLoop() {
+    Render();
+    instance.ProcessEvents();
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Init();
+    emscripten_set_main_loop(MainLoop, 0, true);
     return 0;
 }
